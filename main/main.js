331eeafacfe0bd76aa1d4f57517b8179fdc0b8b7
rainpasttime <2923336451@qq.com>
2017-11-25 16:53:16 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,20 +1,15 @@
 let datbase = require("./datbase.js");
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
-
-function printInventory(inputs)
-{
+module.exports = function printInventory(inputs) {
     let i;
     let result;
     let items;
     let j;
     let promotionItems;
     let totalSum = 0;
-    let totlaSave = 0;
+    let totalSave = 0;
+    let printf="";
 
-    console.log("***<没钱赚商店>购物清单***");
+    printf+="***<没钱赚商店>购物清单***\n";
 
     //获得这次购买的所有商品条码和数量
     result = count_same_elements(inputs);
@@ -33,16 +28,17 @@ function printInventory(inputs)
         {
             if(items[j].barcode===result[i].barcode)
             {
-                totlaSave += save*items[j].price;
+                totalSave += save*items[j].price;
                 let sum = items[j].price*result[i].count - save*items[j].price;
                 totalSum+=sum;
-                console.log("名称："+items[j].name+"，数量："+result[i].count+items[j].unit+"，单价："+items[j].price.toString()+"，小计："+sum+"(元)");
+                printf+="名称："+items[j].name+"，数量："+result[i].count+items[j].unit+"，单价："+changeToFloat(items[j].price)+"(元)"+"，小计："+changeToFloat(sum)+"(元)\n";
             }
         }
     }
 
-    console.log("----------------------");
-    console.log("挥泪赠送商品：");
+    printf+="----------------------\n";
+    printf+="挥泪赠送商品：\n";
+
     for(i=0;i<result.length;i++)
     {
         let save = saveMoney(result[i].barcode,promotionItems,result[i].count);
@@ -52,17 +48,20 @@ function printInventory(inputs)
             {
                 if(items[j].barcode===result[i].barcode)
                 {
-                    console.log("名称："+items[j].name+"，数量："+save+items[j].unit);
+                    printf+="名称："+items[j].name+"，数量："+save+items[j].unit+"\n";
                 }
             }
         }
     }
 
-    console.log("----------------------");
-    console.log("总计："+totalSum+"(元)");
-    console.log("节省："+totlaSave+"(元)");
+    printf+="----------------------\n";
+    printf+="总计："+changeToFloat(totalSum)+"(元)\n";
+    printf+="节省："+changeToFloat(totalSave)+"(元)\n";
+    printf+="**********************";
 
-}
+    console.log(printf);
+
+};
 
 //统计购物清单里面的物品总数
 function count_same_elements(collection) {
@@ -74,8 +73,6 @@ function count_same_elements(collection) {
         let number = 0;
         let k = collection[l];
         subTwo = k.substring(0,10);
-        //console.log("k");
-        //console.log(k);
         if(k.length>10)
         {
             subOne = k.substring(11);
@@ -136,17 +133,29 @@ function saveMoney(barcode,promotionItems,number)
     return 0;
 }
 
-let inputs = [
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000003-2',
-    'ITEM000005',
-    'ITEM000005',
-    'ITEM000005'
-];
+function changeToFloat(number)
+{
+    let stringNumber = number.toString();
+    let i;
+    let tag = false;
+    for(i=0;i<stringNumber.length;i++)
+    {
+        if(stringNumber[i]==='.')
+        {
+            tag = true;
+            if(i+3>stringNumber.length)
+                stringNumber+="0";
 
-printInventory(inputs);
+        }
+        if(i===stringNumber.length-1 && !tag)
+        {
+            stringNumber+=".";
+            stringNumber+="0";
+            stringNumber+="0";
+        }
+    }
+    return stringNumber;
+}
+
+
 
